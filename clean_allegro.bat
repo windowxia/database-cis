80bf4e63a34fc197c9a9c2fe957f4c22074a0bb9
夏兴兵 <windowxia@sina.com>
2023-06-25 22:18:56 +0800

    添加clean文件

---
+++ clean_allegro.bat
@@ -0,0 +1,9 @@
+del /a /f /s *.log
+del /a /f /s *.tag
+del /a /f /s *f*.opj
+del /a /f /s *.jrl*
+del /a /f /s *LOG*
+del /a /f /s signosie*
+del /a /f /s pstchip*
+del /a /f /s pstxnet*
+del /a /f /s pstxprt*
